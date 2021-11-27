642abd2119ab04f8ca0fbf30967ebb3906cbd543
Taehee Lim <lim3617@naver.com>
2021-11-27 17:14:18 +0900

    first commit

---
+++ src/components/List.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <router-link to="/detail">{{ 블로그글[0].title }}</router-link>
+        <p>{{블로그글[0].date}}</p>
+    </div>
+    <div>
+        <h4 @click="$router.push('/detail/1')">{{블로그글[1].title}}</h4>
+        <p>{{블로그글[1].date}}</p>
+    </div>
+    <div>
+        <h4>{{블로그글[2].title}}</h4>
+        <p>{{블로그글[2].date}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'list',
+    props : {
+        블로그글 : Array,
+    }
+
+}
+</script>
+
+<style>
+
+</style>
